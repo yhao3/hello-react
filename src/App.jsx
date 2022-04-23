0c0c4b55323a739f22911ad3f44b51b0bc10dc99
yhao3 <kyrieeeee2@gmail.com>
2022-04-23 22:14:58 +0800

    03_TodoList project ver1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-// 創建 "外殼" 元件 App
 import React, {Component} from 'react';
-import Hello from './components/Hello/Hello'
-
-// 「創建」並「導出」 App 元件
-// class App extends React.Component {
-// import {Component} 後，就可簡寫成: 
-// 注意: import {Xxx} 並非「解構」語法，而是 ES6 新的 import 方式，這種語法意味著使用了「分別導出」ex: export class Component {...}
+import './App.css';
 export default class App extends Component {
     render() {
         return(
-            <div>
-                <Hello />
+            <div className="todo-container">
+                <div className="todo-wrap">
+                <div className="todo-header">
+                    <input type="text" placeholder="請輸入你的任務名稱，按下[Enter]確認"/>
+                </div>
+                <ul className="todo-main">
+                    <li>
+                    <label>
+                        <input type="checkbox"/>
+                        <span>xxxxx</span>
+                    </label>
+                    <button className="btn btn-danger" style={{display:'none'}}>刪除</button>
+                    </li>
+                    <li>
+                    <label>
+                        <input type="checkbox"/>
+                        <span>yyyy</span>
+                    </label>
+                    <button className="btn btn-danger" style={{display:'none'}}>刪除</button>
+                    </li>
+                </ul>
+                <div className="todo-footer">
+                    <label>
+                    <input type="checkbox"/>
+                    </label>
+                    <span>
+                    <span>已完成0</span> / 全部2
+                    </span>
+                    <button className="btn btn-danger">清除已完成任務</button>
+                </div>
+                </div>
             </div>
         )
     }
