09b6f9b860d8b26dfbea79bb27b5f01369071922
yhao3 <kyrieeeee2@gmail.com>
2022-04-24 05:11:11 +0800

    03_TodoList project ver1.6: 新增鼠標移入時 更改 <li> 背景色 並 顯示[刪除]按鈕效果

--- README.md
+++ README.md
@@ -140,4 +140,27 @@
       - 該函式接收一個欲新增的 todoObj，當想傳數據給(父)元件 `App` 時，就調用該函式，將原 todos 狀態更新
       - `App` 狀態更新後，就會呼叫 `App` 中的 render() 函式，由於 `List` 是其子元件，故也導致子元件重新渲染，達成按下 [Enter] 後將列表重新渲染的需求
 
-
+## 3.4 鼠標移入時顯示[刪除]按鈕效果
+### `Item.jsx`
+1. 將 <li> 標籤添加css樣式，並綁定鼠標移入移出事件
+2. 宣告 並 初始化 state: 
+   isMouseEnter: 該 <li> 是否被鼠標指到，用來控制 <li> 之背景色及是否顯示[刪除]按鈕。預設為 false
+   ```jsx
+   state = { isMouseEnter: false }
+   ```
+3. 宣告 onMouseEnter 及 onMouseLeave 兩事件的 callback function
+    ```jsx
+    /**
+     * onMouseEnter 及 onMouseLeave 兩事件的 callback function
+     * 1. 當鼠標移入觸發 onMouseEnter 事件，會傳入 true 參數，修改 Item 的 isMouseEnter 狀態，以控制 <li> 之背景色及是否顯示[刪除]按鈕
+     * 2. 當鼠標移出觸發 onMouseLeave 事件，會傳入 false 參數，修改 Item 的 isMouseEnter 狀態，以控制 <li> 之背景色及是否顯示[刪除]按鈕
+     * @param {*} true_or_false 
+     * @returns 
+     */
+     handleMouse = (true_or_false) => {
+        return () => {
+            this.setState( {isMouseEnter: true_or_false} )
+        }
+     }
+     ```
+4. [刪除]按鈕添加css樣式，根據 isMouseEnter 狀態決定是否顯示
--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -2,16 +2,36 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Item extends Component {
+
+  // 初始化 state: 
+  // isMouseEnter: 該 <li> 是否被鼠標指到，用來控制 <li> 之背景色及是否顯示[刪除]按鈕。預設為 false
+  state = { isMouseEnter: false }
+
+  /**
+   * onMouseEnter 及 onMouseLeave 兩事件的 callback function
+   * 1. 當鼠標移入觸發 onMouseEnter 事件，會傳入 true 參數，修改 Item 的 isMouseEnter 狀態，以控制 <li> 之背景色及是否顯示[刪除]按鈕
+   * 2. 當鼠標移出觸發 onMouseLeave 事件，會傳入 false 參數，修改 Item 的 isMouseEnter 狀態，以控制 <li> 之背景色及是否顯示[刪除]按鈕
+   * @param {*} true_or_false 
+   * @returns 
+   */
+  handleMouse = (true_or_false) => {
+    return () => {
+      this.setState( {isMouseEnter: true_or_false} )
+    }
+  }
+
   render() {
     const {name, done} = this.props;
+    const {isMouseEnter} = this.state;
+
     return (
-        <li>
+        <li style={ {backgroundColor: isMouseEnter ? '#ddd' : 'white'} } onMouseEnter={ this.handleMouse(true) } onMouseLeave={ this.handleMouse(false) } > {/* <-- 添加樣式，並綁定鼠標移入移出事件 */}
             <label>
                 {/* <input type="checkbox" checked={done} /> 這樣寫會變成 read-only */}
                 <input type="checkbox" defaultChecked={done} />
                 <span>{name}</span>
             </label>
-            <button className="btn btn-danger" style={{display:'none'}}>刪除</button>
+            <button className="btn btn-danger" style={ {display: isMouseEnter ? 'block' : 'none'} }>刪除</button>
         </li>
     )
   }
